198fac00a296b96d1ddc678c530e55281f8559b7
Jonathan Lam <jonhaolam@gmail.com>
2024-08-14 18:28:46 -0400

    Added post request handling to generate flashcards with OpenAI

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -15,11 +15,32 @@ Strive for a balance between breadth and depth, covering key points without over
 Return flashcards in the following JSON format:
 
 {
-    "flashcards": {
-        "front": str,
-        "back": str
-    }
+    "flashcards": [
+        {
+            "front": str,
+            "back": str
+        }
+    ]
 }
 `
+
+export async function POST(req) {
+    const openai = OpenAI();
+    const data = await req.text()
+
+    const completion = await openai.chat.completion.create({
+        messages: [
+            {role: "system", content: systemPrompt},
+            {role: "user", content: data}
+        ],
+        model: "gpt-4o",
+        response_format:{type: "json_object"}
+    })
+
+    const flashcards = JSON.parse(completion.choices[0].message.content)
+
+    return NextResponse.json(flashcards.flashcard)
+}   
+
 
 
